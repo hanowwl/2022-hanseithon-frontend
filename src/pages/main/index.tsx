b395355aa2a61c20e02a95d07a5a73f14902cf3f
최민기 <cmg8431@gmail.com>
2022-06-21 14:45:12 +0900

    chore: styled-components emotion으로 변경

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,5 @@
 import React from "react";
 
-export const MainPage: React.FC = () => <div>메인 페이지</div>;
+import * as S from "./styled";
+
+export const MainPage: React.FC = () => <S.Wrapper>메인 페이지</S.Wrapper>;
