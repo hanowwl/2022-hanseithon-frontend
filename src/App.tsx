744050d4d65f1b83144429d06a015b68420ac9cd
최민기 <cmg8431@gmail.com>
2022-06-21 19:05:21 +0900

    feat: auth 페이지 router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { MainPage } from "./pages";
+import {
+  LoginPage,
+  MainPage,
+  RegisterStep1Page,
+  RegisterStep2Page,
+  RegisterStep3Page,
+} from "./pages";
 
 export const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<MainPage />} />
+      <Route path="auth">
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register">
+          <Route path="step1" element={<RegisterStep1Page />} />
+          <Route path="step2" element={<RegisterStep2Page />} />
+          <Route path="step3" element={<RegisterStep3Page />} />
+        </Route>
+        <Route
+          path="register"
+          element={<Navigate replace to="/auth/login" />}
+        />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
