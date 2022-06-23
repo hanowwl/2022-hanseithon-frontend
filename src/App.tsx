4dee4d6afc7142d749eef50ecd6861067a040ea7
최민기 <cmg8431@gmail.com>
2022-06-23 13:17:51 +0900

    refactor: recoil, quert 세팅 index로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 
 import {
   LoginPage,
@@ -11,34 +9,22 @@ import {
   RegisterStep3Page,
 } from "./pages";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 export const App: React.FC = () => (
-  <RecoilRoot>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<MainPage />} />
-          <Route path="auth">
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register">
-              <Route path="step1" element={<RegisterStep1Page />} />
-              <Route path="step2" element={<RegisterStep2Page />} />
-              <Route path="step3" element={<RegisterStep3Page />} />
-            </Route>
-            <Route
-              path="register"
-              element={<Navigate replace to="/auth/login" />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  </RecoilRoot>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<MainPage />} />
+      <Route path="auth">
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register">
+          <Route path="step1" element={<RegisterStep1Page />} />
+          <Route path="step2" element={<RegisterStep2Page />} />
+          <Route path="step3" element={<RegisterStep3Page />} />
+        </Route>
+        <Route
+          path="register"
+          element={<Navigate replace to="/auth/login" />}
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
