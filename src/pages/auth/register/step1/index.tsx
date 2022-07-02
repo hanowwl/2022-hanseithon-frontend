ee9ef9af2cae483438aca0371ca4478987799f98
HanSu Lee <i.hansu.lee@gmail.com>
2022-07-02 20:57:17 +0900

    Revert "Feature/auth register step1"

--- src/pages/auth/register/step1/index.tsx
+++ src/pages/auth/register/step1/index.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useCallback, useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 
 import * as S from "./styled";
 
-type RegisterStep1Values = {
+export type RegisterStep1Values = {
   service: boolean;
   privacy: boolean;
   student: boolean;
@@ -18,28 +19,24 @@ export const RegisterStep1Page: React.FC = () => {
     watch,
     formState: { errors },
   } = useForm<RegisterStep1Values>();
-  
+
   const [isAllChecked, setIsAllChecked] = useState<boolean>(false);
-  const navigate = useNavigate();
+  const onSubmit = ({ service, privacy, student }: RegisterStep1Values) => {};
 
   const allAgreeHandler = useCallback(() => {
-    setIsAllChecked((prev) => !prev);
     if (!isAllChecked) {
+      setIsAllChecked(true);
       reset({ service: true, privacy: true, student: true });
     } else {
+      setIsAllChecked(false);
       reset({ service: false, privacy: false, student: false });
     }
   }, [isAllChecked, reset]);
 
-  const onSubmitHandler = handleSubmit((data: RegisterStep1Values) => {
-    console.log(data);
-  });
-
   useEffect(() => {
     const subscription = watch((value) => {
-      if (value.student && value.service && value.privacy) {
+      if (value.student && value.service && value.privacy)
         return setIsAllChecked(true);
-      }
       return setIsAllChecked(false);
     });
     return () => subscription.unsubscribe();
@@ -47,8 +44,6 @@ export const RegisterStep1Page: React.FC = () => {
 
   return (
     <div>
-
-      <form onSubmit={onSubmitHandler}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="isAllChecked">
@@ -63,6 +58,7 @@ export const RegisterStep1Page: React.FC = () => {
           </label>
           <span>모두 동의합니다</span>
         </div>
+
         <div>
           <label htmlFor="student">
             <S.CircleCheck checked={watch("student")} />
@@ -75,6 +71,7 @@ export const RegisterStep1Page: React.FC = () => {
           <span>재학생이 맞으신가요?</span>
         </div>
         {errors.student?.message}
+
         <div>
           <label htmlFor="service">
             <S.CircleCheck checked={watch("service")} />
@@ -89,6 +86,7 @@ export const RegisterStep1Page: React.FC = () => {
           <span>서비스 이용약관</span>
         </div>
         {errors.service?.message}
+
         <div>
           <label htmlFor="privacy">
             <S.CircleCheck checked={watch("privacy")} />
@@ -103,14 +101,6 @@ export const RegisterStep1Page: React.FC = () => {
           <span>개인정보 수집 이용 동의</span>
         </div>
         {errors.privacy?.message}
-
-        <button
-          type="submit"
-          disabled={!isAllChecked}
-          onClick={() => isAllChecked && navigate("/auth/register/step3")}
-        >
-          다음으로
-        </button>
       </form>
     </div>
   );
