a9a7ada0008af0220a88e3784dbabf3c526f484d
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-03 23:32:31 +0900

    feat: DefaultLayout 레이아웃 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./common";
 export * from "./hackathon";
+export * from "./layout";
---
+++ src/components/layout/DefaultLayout/styled.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const DefaultLayoutContainer = styled.div`
+  width: 100%;
+  max-width: 1220px;
+  margin: 0 auto;
+`;
