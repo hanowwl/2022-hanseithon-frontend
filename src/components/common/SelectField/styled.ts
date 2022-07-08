5493ce8265cfc3271a89a531a45f02ce29d98e5c
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-08 23:25:26 +0900

    feat: SelectField 컴포넌트 추가

---
+++ src/components/common/SelectField/styled.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const SelectElementWrapper = styled.div`
+  background-color: #18181a;
+  border-radius: 0.7rem;
+`;
+
+export const SelectElement = styled.select`
+  border: none;
+  outline: none;
+  color: #dcdcdc;
+  width: 100%;
+  border-radius: 0.7rem;
+  padding: 1.5rem;
+  appearance: none;
+  background: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M19.7071 9.70711C20.0976 9.31658 20.0976 8.68342 19.7071 8.29289C19.3166 7.90237 18.6834 7.90237 18.2929 8.29289L19.7071 9.70711ZM12 16L11.2929 16.7071C11.4804 16.8946 11.7348 17 12 17C12.2652 17 12.5196 16.8946 12.7071 16.7071L12 16ZM5.70711 8.29289C5.31658 7.90237 4.68342 7.90237 4.29289 8.29289C3.90237 8.68342 3.90237 9.31658 4.29289 9.70711L5.70711 8.29289ZM18.2929 8.29289L11.2929 15.2929L12.7071 16.7071L19.7071 9.70711L18.2929 8.29289ZM12.7071 15.2929L5.70711 8.29289L4.29289 9.70711L11.2929 16.7071L12.7071 15.2929Z' fill='white'/%3E%3C/svg%3E%0A")
+    no-repeat right 9px center;
+`;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 export * from "./Button";
 export * from "./Navbar";
 export * from "./Portal";
+export * from "./SelectField";
 export * from "./TextField";
