55f1821ba13845f5ee50dfe15ed4895cc8809e81
최민기 <cmg8431@gmail.com>
2022-07-11 10:45:59 +0900

    feat: GlobalStyle Color Scheme 추가

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,6 +2,8 @@ import { css } from "@emotion/react";
 
 export const globalStyles = css`
   :root {
+    color-scheme: dark;
+
     --color-primary: #4b5fff;
     --color-primary-darker: #3047ff;
     --color-primary-gray: #959595;
