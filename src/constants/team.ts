cbec80ff5e2aec1c19646b796e2e52a004880a13
최민기 <cmg8431@gmail.com>
2022-07-11 10:45:01 +0900

    feat: Team Type 정보 추가

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -13,3 +13,22 @@ export const TEAM_TYPE_VALUES = {
 } as const;
 
 export type TEAM_TYPE = typeof TEAM_TYPE_VALUES[keyof typeof TEAM_TYPE_VALUES];
+
+export type TeamCategroyType = {
+  [key: string]: string | undefined;
+};
+
+export const TeamCategroy: TeamCategroyType = {
+  DEVELOPER: "개발자",
+  DESIGN: "디자이너",
+  PRODUTMANGER: "PM",
+} as const;
+
+export type TeamPartType = {
+  [key: string]: string | undefined;
+};
+
+export const TeamPart: TeamPartType = {
+  LIVING: "생활",
+  GAME: "게임",
+} as const;
