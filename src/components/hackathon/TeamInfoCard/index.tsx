40ca90806ff9eabc31f1bd6b0fef236d16b48f6e
최민기 <cmg8431@gmail.com>
2022-07-11 10:40:46 +0900

    feat: Api 데이터 적용 및 반응형 구현

--- src/components/hackathon/TeamInfoCard/index.tsx
+++ src/components/hackathon/TeamInfoCard/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 
-import * as S from "./styled";
+import { UserNameResponse } from "src/api/user";
+import { TeamPart } from "src/constants";
 
-export type TeamType = "game" | "living" | string;
+import * as S from "./styled";
 
 export interface TeamInfoCardProps {
   name: string;
-  type: TeamType;
+  type: "GAME" | "LIVING";
+  owner: { name: string };
   description: string;
-  owner: string;
-  members: string[];
+  members: UserNameResponse[];
 }
 
 export const TeamInfoCard: React.FC<TeamInfoCardProps> = ({
   name,
   type,
-  description,
   owner,
+  description,
   members,
 }) => (
   <S.TeamInfoCardContainer>
-    <S.TeamInfoCardTypeBox>{type}</S.TeamInfoCardTypeBox>
+    <S.TeamInfoCardTypeBox>{TeamPart[type]}</S.TeamInfoCardTypeBox>
     <div>
       <S.TeamInfoCardTitle>{name}</S.TeamInfoCardTitle>
       <S.TeamInfoCardDescription>{description}</S.TeamInfoCardDescription>
@@ -29,18 +30,23 @@ export const TeamInfoCard: React.FC<TeamInfoCardProps> = ({
           <S.TeamInfoCardUserMembersTitle>
             팀장 : &nbsp;
           </S.TeamInfoCardUserMembersTitle>
-          <S.TeamInfoCardUserMembersBox>{owner}</S.TeamInfoCardUserMembersBox>
+          <S.TeamInfoCardUserMembersBox>
+            {owner.name}
+          </S.TeamInfoCardUserMembersBox>
         </S.TeamInfoCardUserContainer>
         <S.TeamInfoCardUserContainer>
           <S.TeamInfoCardUserMembersTitle>
             팀원 : &nbsp;
           </S.TeamInfoCardUserMembersTitle>
           <S.TeamInfoCardUserMembersContainer>
-            {members.map((member) => (
-              <S.TeamInfoCardUserMembersBox>
-                {member}
-              </S.TeamInfoCardUserMembersBox>
-            ))}
+            {members.map(
+              (member) =>
+                owner.name !== member.name && (
+                  <S.TeamInfoCardUserMembersBox>
+                    {member.name}
+                  </S.TeamInfoCardUserMembersBox>
+                ),
+            )}
           </S.TeamInfoCardUserMembersContainer>
         </S.TeamInfoCardUserContainer>
       </div>
