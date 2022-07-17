e8567a33a44e7fac8b4cb846c575136a529c40e7
Comet105 <taegyeon512@naver.com>
2022-07-17 23:36:46 +0900

    feat: competition form 구현

--- src/pages/main/competition/index.tsx
+++ src/pages/main/competition/index.tsx
@@ -3,11 +3,110 @@ import React from "react";
 import * as S from "./styled";
 
 export const Competition: React.FC = () => (
-  <S.CompetitionContainer>
-    <S.CompetitionWrapper>
-      <S.CompetitionReadyText>
-        아직 준비중이에요 <br /> 기다려주세요!
-      </S.CompetitionReadyText>
-    </S.CompetitionWrapper>
-  </S.CompetitionContainer>
+  <div>
+    <div>
+      <table>
+        <S.CompetitionTableTheadWrapper>
+          <td>부문</td>
+          <td>심사 항목</td>
+          <td>심사 기준</td>
+          <td>배 점</td>
+        </S.CompetitionTableTheadWrapper>
+
+        <S.CompetitionTabletest>
+          <div>
+            <td>생활</td>
+            <td>
+              <tr>
+                <td>기획</td>
+                <td>
+                  <tr>독창성</tr>
+                  <tr>확장성</tr>
+                  <tr>주제확장성 + 타당성</tr>
+                  <tr>발표</tr>
+                </td>
+                <td>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                </td>
+              </tr>
+              <tr>
+                <td>개발</td>
+                <td>
+                  <tr>최적화</tr>
+                  <tr>구현완성도</tr>
+                </td>
+                <td>
+                  <tr>10</tr>
+                  <tr>20</tr>
+                </td>
+              </tr>
+              <tr>
+                <td>디자인</td>
+                <td>
+                  <tr>편의성</tr>
+                  <tr>직관성</tr>
+                  <tr>심미성</tr>
+                </td>
+                <td>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                </td>
+              </tr>
+            </td>
+          </div>
+
+          <div>
+            <td>게임</td>
+            <td>
+              <tr>
+                <td>기획</td>
+                <td>
+                  <tr>독창성</tr>
+                  <tr>확장성</tr>
+                  <tr>주제확장성 + 타당성</tr>
+                  <tr>발표</tr>
+                </td>
+                <td>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                  <tr>10</tr>
+                </td>
+              </tr>
+              <tr>
+                <td>개발</td>
+                <td>
+                  <tr>최적화</tr>
+                  <tr>구현완성도</tr>
+                </td>
+                <td>
+                  <tr>10</tr>
+                  <tr>20</tr>
+                </td>
+              </tr>
+              <tr>
+                <td>디자인</td>
+                <td>
+                  <tr>심미성</tr>
+                  <tr>창의성</tr>
+                  <tr>직관성</tr>
+                  <tr>편의성</tr>
+                </td>
+                <td>
+                  <tr>8</tr>
+                  <tr>8</tr>
+                  <tr>7</tr>
+                  <tr>7</tr>
+                </td>
+              </tr>
+            </td>
+          </div>
+        </S.CompetitionTabletest>
+      </table>
+    </div>
+  </div>
 );
