7e5afdc72993062a8907a479d39b662e7e1d8a95
Comet105 <taegyeon512@naver.com>
2022-07-18 10:33:59 +0900

    refactor: competition form 리펙토링

--- src/pages/main/competition/index.tsx
+++ src/pages/main/competition/index.tsx
@@ -1,112 +1,103 @@
 import React from "react";
 
-import * as S from "./styled";
+// import * as S from "./styled";
 
 export const Competition: React.FC = () => (
   <div>
-    <div>
-      <table>
-        <S.CompetitionTableTheadWrapper>
-          <td>부문</td>
-          <td>심사 항목</td>
-          <td>심사 기준</td>
-          <td>배 점</td>
-        </S.CompetitionTableTheadWrapper>
+    <table border={1} align="center" width="100%">
+      <thead>
+        <td>부문</td>
+        <td>심사 항목</td>
+        <td>심사 기준</td>
+        <td>배 점</td>
+      </thead>
+      <tbody>
+        <tr>
+          <td rowSpan={9}>생활</td>
+          <td rowSpan={4}>기획</td>
+          <td>독창성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>확장성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>주제적합성 + 타당성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>발표</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td rowSpan={2}>개발</td>
+          <td>최적화</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>구현완성도</td>
+          <td>20</td>
+        </tr>
+        <tr>
+          <td rowSpan={3}>디자인</td>
+          <td>편의성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>직관성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>심미성</td>
+          <td>10</td>
+        </tr>
 
-        <S.CompetitionTableTbodyWrapper>
-          <div>
-            <td>생활</td>
-            <td>
-              <tr>
-                <td>기획</td>
-                <td>
-                  <tr>독창성</tr>
-                  <tr>확장성</tr>
-                  <tr>주제확장성 + 타당성</tr>
-                  <tr>발표</tr>
-                </td>
-                <td>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                </td>
-              </tr>
-              <tr>
-                <td>개발</td>
-                <td>
-                  <tr>최적화</tr>
-                  <tr>구현완성도</tr>
-                </td>
-                <td>
-                  <tr>10</tr>
-                  <tr>20</tr>
-                </td>
-              </tr>
-              <tr>
-                <td>디자인</td>
-                <td>
-                  <tr>편의성</tr>
-                  <tr>직관성</tr>
-                  <tr>심미성</tr>
-                </td>
-                <td>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                </td>
-              </tr>
-            </td>
-          </div>
-
-          <div>
-            <td>게임</td>
-            <td>
-              <tr>
-                <td>기획</td>
-                <td>
-                  <tr>독창성</tr>
-                  <tr>확장성</tr>
-                  <tr>주제확장성 + 타당성</tr>
-                  <tr>발표</tr>
-                </td>
-                <td>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                  <tr>10</tr>
-                </td>
-              </tr>
-              <tr>
-                <td>개발</td>
-                <td>
-                  <tr>최적화</tr>
-                  <tr>구현완성도</tr>
-                </td>
-                <td>
-                  <tr>10</tr>
-                  <tr>20</tr>
-                </td>
-              </tr>
-              <tr>
-                <td>디자인</td>
-                <td>
-                  <tr>심미성</tr>
-                  <tr>창의성</tr>
-                  <tr>직관성</tr>
-                  <tr>편의성</tr>
-                </td>
-                <td>
-                  <tr>8</tr>
-                  <tr>8</tr>
-                  <tr>7</tr>
-                  <tr>7</tr>
-                </td>
-              </tr>
-            </td>
-          </div>
-        </S.CompetitionTableTbodyWrapper>
-      </table>
-    </div>
+        <tr>
+          <td rowSpan={10}>게임</td>
+          <td rowSpan={4}>기획</td>
+          <td>독창성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>재미 요소</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>주제적합성 + 타당성</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>발표</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td rowSpan={2}>개발</td>
+          <td>최적화</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>구현완성도</td>
+          <td>20</td>
+        </tr>
+        <tr>
+          <td rowSpan={4}>디자인</td>
+          <td>심미성</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>창의성</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>직관성</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>편의성</td>
+          <td>7</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 );
